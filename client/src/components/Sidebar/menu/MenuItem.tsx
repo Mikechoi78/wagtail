benmoumene/Django-content-management-system

Initial work on new sidebar react/css

client/src/components/Sidebar/menu/MenuItem.tsx
@@ -1,0 +1,27 @@
+import { MenuAction, MenuState } from '../modules/MainMenu';
+
+export interface MenuItemRenderContext {
+    path: string;
+    state: MenuState;
+    collapsed: boolean;
+    dispatch(action: MenuAction);
+    navigate(url: string): Promise<void>;
+}
+
+export interface MenuItemDefinition {
+    name: string;
+    label: string;
+    iconName: string | null;
+    classNames?: string;
+    render(context: MenuItemRenderContext): React.ReactFragment;
+}
+
+export interface MenuItemProps<T> {
+    path: string;
+    state: MenuState;
+    item: T;
+    collapsed: boolean;
+    dispatch(action: MenuAction): void;
+    navigate(url: string): Promise<void>;
+}
+

Mikechoi78/wagtail

client/src/components/Sidebar/menu/MenuItem.tsx
@@ -1,0 +1,26 @@
+import { MenuAction, MenuState } from '../modules/MainMenu';
+
+export interface MenuItemRenderContext {
+  path: string;
+  state: MenuState;
+  slim: boolean;
+  dispatch(action: MenuAction);
+  navigate(url: string): Promise<void>;
+}
+
+export interface MenuItemDefinition {
+  name: string;
+  label: string;
+  iconName: string | null;
+  classNames?: string;
+  render(context: MenuItemRenderContext): React.ReactFragment;
+}
+
+export interface MenuItemProps<T> {
+  path: string;
+  state: MenuState;
+  item: T;
+  dispatch(action: MenuAction): void;
+  navigate(url: string): Promise<void>;
+}
+